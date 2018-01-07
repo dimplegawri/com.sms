0ee1cab4307e89c03ce85cae444e6f4f5ec00e4f
shiv.ve <shiv.ve@LP-5CD719933P.HCLT.CORP.HCL.IN>
2018-01-08 00:39:25 +0530

    Add document comment

--- src/main/java/com/ska/sms/SmsProjectApplication.java
+++ src/main/java/com/ska/sms/SmsProjectApplication.java
@@ -3,6 +3,11 @@ package com.ska.sms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * @author shiv.ve
+ * @version v.1.0
+ */
 @SpringBootApplication
 public class SmsProjectApplication {
 
